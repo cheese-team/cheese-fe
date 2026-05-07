483193475f91f84bd60779992186050f76b8b32c
ozen0718 <117171401+ozen0718@users.noreply.github.com>
2026-05-07 14:42:13 +0900

    Revert "Feat: 검색, 카테고리 공통 컴포넌트 작성"

--- src/components/common/CategoryTabs/index.ts
+++
@@ -1,4 +0,0 @@
-export { default } from './CategoryTabs';
-export { default as CategoryTabs } from './CategoryTabs';
-
-export type { CategoryTabItem, CategoryTabsProps, CategoryTabsSize } from './type';
--- src/components/common/ListFilterBar/index.ts
+++
@@ -1,4 +0,0 @@
-export { default } from './ListFilterBar';
-export { default as ListFilterBar } from './ListFilterBar';
-
-export type { ListFilterActionButton, ListFilterBarProps, ListFilterSortOption } from './type';
